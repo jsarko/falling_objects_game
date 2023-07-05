c8038f31758c6c2284c1b83f3aa8c7f02c4e41ee
jsarko <36605206+jsarko@users.noreply.github.com>
2023-07-05 15:51:02 -0500

    Implemented a base class and sub classes for objects

--- models.py
+++ models.py
@@ -1,23 +1,28 @@
-from pygame import Vector2
+import pygame
 
-class BaseObject:
-    self.pos = pygame.Vector2(
-        screen.get_width() / 2, 
-        screen.get_height() /2
-    )
-
-    def move(self):
-        pass
+class GameObject(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        super().__init__()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.image = image
     
-    def draw(self):
+    def update(self):
+        raise NotImplementedError("Subclasses must implement the update method.")
+
+
+class Player(GameObject):
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y)
+    
+    def update(self):
         pass
 
-class Player(BaseObject):
-    self.player_pos = pygame.Vector2(
-        screen.get_width() / 2, 
-        screen.get_height() - 150
-    )
-    self.player_img = pygame.transform.scale(
-        pygame.image.load("assets/pixel_bubz_64.png"), 
-        (150, 150)
-    )
+class FallingObject(GameObject):
+    def __init__(self, image, x, y):
+        super().__init__()
+    
+    def update(self):
+        pass
+
